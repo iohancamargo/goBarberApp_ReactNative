1a9f506e712000a59d70a701754d2605c169f59d
Iohan Camargo <iohanutfpr@gmail.com>
2020-09-15 21:11:59 -0300

    Inserindo validacoes para formularios

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,8 @@
 import React, {
   useEffect,
   useRef,
+  useState,
+  useCallback,
   useImperativeHandle,
   forwardRef,
 } from 'react';
@@ -29,6 +31,17 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   const inputElementRef = useRef<any>(null);
   const { registerField, defaultValue, fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
 
   useImperativeHandle(ref, () => ({
     focus() {
@@ -53,8 +66,12 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused} isErrored={!!error}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
       <TextInput
         ref={inputElementRef}
         onChangeText={(value) => {
@@ -62,6 +79,8 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
         }}
         keyboardAppearance="dark"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         placeholderTextColor="#666360"
         {...rest}
       />
